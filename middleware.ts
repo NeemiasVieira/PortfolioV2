c00b3056307908247eb8639d6d56d4a4bc22ac94
Neemias Vieira <neemias.vieira@live.com>
2025-02-13 10:27:06 -0300

    fix: removing redirect on locale middleware

--- middleware.ts
+++ middleware.ts
@@ -6,24 +6,18 @@ const defaultLocale = 'pt-BR';
 export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
 
-  // Verifica se o caminho já inclui um idioma válido
   if (locales.some((locale) => pathname.startsWith(`/${locale}`))) {
     return NextResponse.next();
   }
 
-  // Obtém o idioma do cabeçalho 'Accept-Language', ou usa o idioma padrão
   const userLang = req.headers.get('accept-language')?.split(',')[0] || defaultLocale;
   const locale = locales.includes(userLang) ? userLang : defaultLocale;
 
-  // Define um cookie para armazenar o idioma
   const response = NextResponse.next();
   response.cookies.set('locale', locale, {
-    path: '/', // Disponível em todas as páginas
-    maxAge: 60 * 60 * 24 * 365, // 1 ano
+    path: '/',
+    maxAge: 60 * 60 * 24 * 365,
   });
-
-  // Redireciona para a URL com o idioma detectado
-  return NextResponse.redirect(new URL(`/${locale}${pathname}`, req.url));
 }
 
 export const config = {
